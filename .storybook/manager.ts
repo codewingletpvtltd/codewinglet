2feb40c13d415752dbf19f0a864a0345dc7844a8
skyadacw <skyada@codewinglet.com>
2023-07-21 19:51:19 +0530

    [CLW-13] added primary and secondary color

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -4,6 +4,9 @@ import { create } from "@storybook/theming";
 const theme = create({
   base: "light",
 
+  colorPrimary: "#0278BE",
+  colorSecondary: "#02457C",
+
   brandTitle: "CodeWinglet",
   brandUrl: "https://www.codewinglet.com/",
   brandImage: "/logo.svg",
