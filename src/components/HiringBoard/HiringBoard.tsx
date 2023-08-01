453eeae749b2315e4f788a5d6488920257aac2fb
Pooja Kahar <pkahar@codewinglet.com>
2023-08-01 10:43:18 +0530

    CLW-15: requested changes

--- src/components/HiringBoard/HiringBoard.tsx
+++ src/components/HiringBoard/HiringBoard.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import Image from 'next/image';
-import HiringImg from '../../assets/hiring.svg';
-import HiringBoardBg from '../../assets/hiringBoard.svg';
+import HiringImg from 'assets/hiring.svg';
+import HiringBoardBg from 'assets/hiringBoard.svg';
 import { HiringBoardProps } from './types';
 
 const HiringBoard: FC<HiringBoardProps> = ({
@@ -10,11 +10,11 @@ const HiringBoard: FC<HiringBoardProps> = ({
   className = '',
 }) => (
   <div>
-    <Image src={HiringImg} alt='' className='ml-[1.9rem]' />
+    <Image src={HiringImg} alt='Hiring Image' className='ml-[1.9rem]' />
     <figure
       className={`absolute top-[4.8rem] left-[20px] animate-swing transform origin-[center_-12px] float-left ${className}`}
     >
-      <Image src={HiringBoardBg} alt='' height={150} width={150} />
+      <Image src={HiringBoardBg} alt='Hiring Board' height={150} width={150} />
       <button
         className='absolute top-[30%] left-[0] p-2 rotate-[-15deg] flex items-center justify-center text-white'
         onClick={onClick}
