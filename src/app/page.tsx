76bd9d78090b975b24f6edd3ca3499b590346ddc
gtanchak <gtanchak@codewinglet.com>
2023-08-04 14:04:40 +0530

    CLW-24: refactor button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-'use client';
+import { Button } from '@codewinglet/components';
 
 export default function Home() {
   return (
     <div>
-      Home Page Y y
-      <h1 className='font-secondary text-h1 font-700'>
-        font-family: Bespoke Sans
-      </h1>
-      <h1 className='font-primary'>primary font family</h1>
+      <Button>Hello worlds</Button>
     </div>
   );
 }
