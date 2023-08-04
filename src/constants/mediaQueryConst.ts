4e906330e10dd29b01516eb8379dd62b84820499
abhishekjoshiCW <ajoshi@codewinglet.com>
2023-08-04 17:13:34 +0530

    mediaQuery Const Refactored

--- src/constants/mediaQueryConst.ts
+++ src/constants/mediaQueryConst.ts
@@ -1,7 +1,7 @@
 const { theme } = require('../../tailwind.config');
 
 export const xs = `(max-width: ${theme.screens.xs})`; //Effects will be applied when the viewport width is 375px or less.
-export const sm = `(min-width: ${theme.screens.sm}) and (max-width: ${theme.screens.md})`; // 768px to 1024px
-export const md = `(min-width: ${theme.screens.md}) and (max-width: ${theme.screens.lg})`; //1024px to 1440px
-export const lg = `(min-width: ${theme.screens.lg}) and (max-width: ${theme.screens.xl})`; //1440px to 1920px
-export const xl = `(min-width: ${theme.screens.xl})`; //Effects will be applied when the viewport width is 1920px or More
+export const sm = `(max-width: ${theme.screens.sm})`;
+export const md = `(max-width: ${theme.screens.md})`;
+export const lg = `(max-width: ${theme.screens.lg})`;
+export const xl = `(max-width: ${theme.screens.xl})`;
