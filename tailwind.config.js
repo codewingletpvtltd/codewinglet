5db9d3c95672752083648e69ee1f41363cebdefa
gtanchak <gtanchak@codewinglet.com>
2023-08-09 15:04:36 +0530

    CLW-52: Change font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -64,77 +64,67 @@ module.exports = {
       900: 900,
     },
     fontFamily: {
-      primary: ['BespokeSans-Variable', 'sans-serif'],
-      secondary: ['CabinetGrotesk-Variable', 'sans-serif'],
+      primary: ['Inter', 'sans-serif'],
+      secondary: ['Outfit', 'sans-serif'],
     },
     fontSize: {
       h1: [
         '55px',
         {
           fontWeight: 800,
-          lineHeight: '68px',
         },
       ],
       h2: [
         '50px',
         {
-          lineHeight: '62px',
           fontWeight: 800,
         },
       ],
       h3: [
         '40px',
         {
-          lineHeight: '50px',
           fontWeight: 900,
         },
       ],
       h4: [
         '25px',
         {
-          lineHeight: '30px',
           fontWeight: 800,
         },
       ],
       h5: [
         '20px',
         {
-          lineHeight: '30px',
           fontWeight: 800,
         },
       ],
       h6: [
         '16px',
         {
-          lineHeight: '25px',
           fontWeight: 400,
         },
       ],
       body1: [
         '20px',
         {
-          lineHeight: '30px',
           fontWeight: 500,
         },
       ],
       body2: [
         '18px',
         {
-          lineHeight: '30px',
           fontWeight: 500,
         },
       ],
       subtitle1: [
         '16px',
         {
-          lineHeight: '22px',
           fontWeight: 400,
         },
       ],
       subtitle2: [
         '14px',
         {
-          lineHeight: '22px',
           fontWeight: 400,
         },
       ],
