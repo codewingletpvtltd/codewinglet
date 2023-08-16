07c5917ba0456a7a839057fc1677591eaf3571a2
Pooja Kahar <pkahar@codewinglet.com>
2023-08-16 11:59:49 +0530

    [CLW-32]: requested changes

--- src/components/OurPortfolio/components/Heading/Heading.tsx
+++ src/components/OurPortfolio/components/Heading/Heading.tsx
@@ -1,9 +1,9 @@
 import { FC } from 'react';
 import { RightArrow } from '@codewinglet/assets';
 import Typography from '../../../Typography';
-import { HeaderProps } from '../../types';
+import { HeadingProps } from '../../types';
 
-const Header: FC<HeaderProps> = ({ title, description }) => (
+const Heading: FC<HeadingProps> = ({ title, description }) => (
   <div className='flex row items-center justify-between mt-[29px] mb-[17px]'>
     <div>
       <Typography variant='subtitle1' className='!font-[800]'>
@@ -17,4 +17,4 @@ const Header: FC<HeaderProps> = ({ title, description }) => (
   </div>
 );
 
-export default Header;
+export default Heading;
--- src/components/OurPortfolio/types.ts
+++ src/components/OurPortfolio/types.ts
@@ -1,4 +1,4 @@
-export interface HeaderProps {
+export interface HeadingProps {
   title: string;
   description?: string;
 }
