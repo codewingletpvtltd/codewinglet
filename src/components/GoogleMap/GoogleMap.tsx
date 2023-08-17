d5674e6bba3aa1d8ebb6ac080e2dc01d0833e256
Pooja Kahar <pkahar@codewinglet.com>
2023-08-17 13:09:05 +0530

    [CLW-41]: Google map height change

--- src/components/GoogleMap/GoogleMap.tsx
+++ src/components/GoogleMap/GoogleMap.tsx
@@ -1,10 +1,10 @@
 const GoogleMap = () => (
-  <div className='relative text-right h-[700px] w-[100%]'>
-    <div className='overflow-hidden !bg-none h-[700px] w-[100%]'>
+  <div className='relative text-right w-[100%] h-[306px] md:h-[400px] lg:h-[470px]'>
+    <div className='overflow-hidden !bg-none w-[100%] h-[306px] md:h-[400px] lg:h-[470px]'>
       <iframe
-        src='https://maps.google.com/maps?q=21.189578, 72.789179&z=19&iwloc=near&output=embed'
+        src='https://maps.google.com/maps?q=21.189578, 72.789179&z=17&output=embed'
         id='gmap_canvas'
-        className='w-[100%] h-[700px]'
+        className='w-[100%] h-[306px] md:h-[400px] lg:h-[470px]'
       ></iframe>
     </div>
   </div>
