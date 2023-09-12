4b45f6c6cf4e410dd90ec6c80bd1780326fcb3ac
Pooja Kahar <pkahar@codewinglet.com>
2023-09-12 17:53:37 +0530

    [CLW-69]: Changes for navigation menu

--- src/components/Header/components/NavigationMenu/MenuIndicator.tsx
+++ src/components/Header/components/NavigationMenu/MenuIndicator.tsx
@@ -1,7 +1,7 @@
 import { memo } from 'react';
 
 const MenuIndicator = () => (
-  <div className='absolute h-[20px] w-[20px] bg-white transform rotate-[45deg] left-9 shadow-lg -z-10 hidden group-hover:block group-hover:animate-enterFromTop' />
+  <div className='absolute h-[25px] w-[25px] mt-[21px] bg-white transform rotate-[45deg] left-9 shadow-lg -z-10 hidden group-hover:block group-hover:animate-enterFromTop' />
 );
 
 export default memo(MenuIndicator);
