f89337a1ed45e5f89764812374e5e34b6bac0291
cw-gtanchak <gtanchak77@codewinglet.com>
2023-09-12 17:55:02 +0530

    [CLW-70]: Added client review

--- src/components/ClientTestimonials/constants.ts
+++ src/components/ClientTestimonials/constants.ts
@@ -2,28 +2,28 @@ export const testimonials = [
   {
     id: 1,
     comment:
-      'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      'Exceptional experience working with Codewinglet software development company! Their team delivered a top-notch product on time and within budget. Their communication and technical expertise made the entire process smooth and efficient.',
     client: {
-      name: 'Sean Agnew',
-      designation: 'CEO at Codewinglet',
+      name: 'Craig Nelson',
+      country: 'USA',
     },
   },
   {
     id: 2,
     comment:
-      'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      'If you need a company to create your next IT product, I would like to tell you about Codewinglet Private Limited. They excel in both quantity and quality. Their process and documentation are very professional. The support I got from them was so good that I can not imagine moving forward without Team Codewinglet.',
     client: {
-      name: 'Sean Agnew',
-      designation: 'CEO at Codewinglet',
+      name: 'Zuhayr Rashid',
+      country: 'UAE',
     },
   },
   {
     id: 3,
     comment:
-      'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      'A great company with teams that could handle all the locations to bring my project to life. Working with them was an awesome experience, and because of them, my projects became a reality. They were dependable, grasped the business well, and had good organization.',
     client: {
-      name: 'Sean Agnew',
-      designation: 'CEO at Codewinglet',
+      name: 'Ross Gates',
+      country: 'USA',
     },
   },
 ];
