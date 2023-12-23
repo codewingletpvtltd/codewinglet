be622b54a73dad799f441fa7bb938251d8ffb93c
bhautikmaniya86 <bhautikmaniya86@gmail.com>
2023-12-23 11:10:47 +0530

    fix: remove sales number

---
+++ src/app/jobs/page.tsx
@@ -0,0 +1,12 @@
+export default function Jobs() {
+  return (
+    <div className='mt-24'>
+      <iframe
+        src='https://codewinglet.oorwin.com/careers/index.html'
+        width='100%'
+        height='2000'
+        style={{ border: 'none', margin: 0, marginTop: '50px' }}
+      ></iframe>
+    </div>
+  );
+}
