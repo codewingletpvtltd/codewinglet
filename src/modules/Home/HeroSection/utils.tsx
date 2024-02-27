f3229677c0c23901aff85d89c932ca19a3c95d3a
cw-gtanchak <gtanchak@codewinglet.com>
2024-02-27 14:28:56 +0530

    CW-129: fix the slider issue for technology

--- src/modules/Home/HeroSection/utils.tsx
+++ src/modules/Home/HeroSection/utils.tsx
@@ -14,86 +14,70 @@ import {
   docker,
 } from '@codewinglet/assets';
 
-export const settings = {
-  initialSlide: 0,
-  slidesToShow: 8,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 6,
-        initialSlide: 0,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 5,
-        initialSlide: 0,
-      },
-    },
-    {
-      breakpoint: 640,
-      settings: {
-        slidesToShow: 3,
-        initialSlide: 0,
-      },
-    },
-  ],
-};
-
 export const techList = [
   {
     id: 1,
     image: react,
+    alt: 'React',
   },
   {
     id: 2,
     image: vuejs,
+    alt: 'VueJs',
   },
   {
     id: 3,
     image: angular,
+    alt: 'Angular',
   },
   {
     id: 4,
     image: postgreSQL,
+    alt: 'PostgreSQL',
   },
   {
     id: 5,
     image: javaScript,
+    alt: 'Javascript',
   },
   {
     id: 6,
     image: nextjs,
+    alt: 'NextJs',
   },
   {
     id: 7,
     image: typescript,
+    alt: 'Typescript',
   },
   {
     id: 8,
     image: nodejs,
+    alt: 'NodeJs',
   },
   {
     id: 9,
     image: php,
+    alt: 'PHP',
   },
   {
     id: 10,
     image: laravel,
+    alt: 'Laravel',
   },
   {
     id: 11,
     image: mySQL,
+    alt: 'mySQL',
   },
   {
     id: 12,
     image: codeigniter,
+    alt: 'Codeigniter',
   },
   {
-    id: 12,
+    id: 13,
     image: docker,
+    alt: 'Docker',
   },
 ];
