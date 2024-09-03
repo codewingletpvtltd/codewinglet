e5eb82209d4e5628866ec0cb08700fb536a6c4f3
cw-gtanchak <gtanchak@codewinglet.com>
2024-09-03 15:31:33 +0530

    feat: add event form

---
+++ src/app/event-form/page.tsx
@@ -0,0 +1,12 @@
+export default function EventPage() {
+  return (
+    <section className='mt-24'>
+      <iframe
+        className='grayscale-0'
+        src='https://airtable.com/embed/appJikY8nTcXhxexH/pagWJ2rXJ5H3hqV05/form'
+        width='100%'
+        height='1600'
+      ></iframe>
+    </section>
+  );
+}
